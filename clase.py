8509a348e73d8c843232961cf0b58cd75c4400c6
GenesisAB2007 <astuagenesis1304@gmail.com>
2024-10-24 20:17:53 -0600

    Commit Inicial

---
+++ clase.py
@@ -0,0 +1,25 @@
+#print palabra reservada
+print("Hola Mundo")
+
+# input es para solicitar datos
+"""input("Digite su nombre: ")
+
+#variables, almacenan informacion
+nombre = "Gen"
+
+print("Hola "+nombre)"""
+
+#Paso 1 asignar nombre
+#Asignación de datos
+#Tipos de datos
+#String cadena de caracteres letras/ numeros/ simbolos
+#Int ---- numeros
+# Float ----- números con decimales
+#boleanos ---- True False
+edad = 0
+nombre = ""
+estatura = 0.0
+esPar = True
+
+print(edad,nombre,estatura,esPar)
+
